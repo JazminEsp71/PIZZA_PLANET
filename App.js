8b026b5650c35370e0b2c754bd188737394ef6b3
Jazmin Esparza <jazminesp71@gmail.com>
2025-02-28 08:25:03 -0600

    creación de vista login

--- App.js
+++ App.js
@@ -1,10 +1,11 @@
 import { StatusBar } from 'expo-status-bar';
 import { StyleSheet, Text, View } from 'react-native';
+import LoginForm from './src/components/screens/Loginform';
 
 export default function App() {
   return (
     <View style={styles.container}>
-      <Text>Open up App.js to start working on your app!</Text>
+      <LoginForm/>
       <StatusBar style="auto" />
     </View>
   );
